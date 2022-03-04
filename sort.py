5bbc9f05346c0e596b626bcb6b04a5a89de73899
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-04 15:37:22 -0800

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,18 @@
+def sort_list(list):
+	n = len(list)
+	i = 0
+	while i < n:
+		k = 0
+		while k < (n-i-1):
+			if(list[k] > list[k+1]):
+				temp = list[k]
+				list[k] = list[k+1]			
+				list[k+1] = temp
+			k+=1
+		i+=1
+	return list
+
+testList = [0, 4, 2, 3]
+testList2 = [3, 2, 4, 89]
+print(sort_list(testList))
+print(sort_list(testList2))
