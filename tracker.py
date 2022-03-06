abe8f722a68d23cd65c0ab02432f02240efa4fb7
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-06 15:44:44 -0800

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,6 +1,6 @@
 def func_counter(func):
-        def wrapper():
-                counter += 1
-                func()
         counter = 0
+        def wrapper(x):
+                counter += 1
+                func(x)
         return wrapper
