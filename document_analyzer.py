f684d36c48468933f848c00d6227e15d2c729fc1
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-09 21:23:21 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -10,19 +10,7 @@ with open('document.txt','r') as f:
 # (print(key, value) for (key, value) in sorted(wordDict.items(), key=lambda x: x[1],reverse=True))
 
 sorted_dict = sorted(wordDict.items(), key=lambda x:x[1], reverse=True)
-"""
 
-sorted_items = sorted_dict.items()
-top_five = list(sorted_items)[:5]
-
-sorted_list = []
-sorted_list.append(sorted_dict.copy())
-
-for i in range(5):
-        print(sorted_list[i])
-
-print(sorted_dict[0])
-"""
 print()
 for i in sorted_dict[0:5]:
 	print(i[0],':', i[1])
