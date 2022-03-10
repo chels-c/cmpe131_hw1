afc6dc17e409f7df5d24130367a956eb686e0ba1
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-09 22:35:30 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -12,6 +12,6 @@ dict_items = wordDict.items()
 sorted_items = sorted(dict_items)
 sorted_dict = sorted(sorted_items, key=lambda x:x[1], reverse=True)
 
-newSort = {}
+print()
 for i in sorted_dict[0:5]:
         print(i[0], ":", i[1])
