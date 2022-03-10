795bd05c1df8c09487d54cd96f5f3d77856fd5ed
chels-c <94879318+chels-c@users.noreply.github.com>
2022-03-09 22:34:24 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,16 +1,17 @@
 wordDict = {}
 
 with open('document.txt','r') as f:
-	for line in f:
-		for word in line.split():
-			if word in wordDict:
-				wordDict[word] += 1
-			else:
-				wordDict[word] = 1
-# (print(key, value) for (key, value) in sorted(wordDict.items(), key=lambda x: x[1],reverse=True))
+        for line in f:
+                for word in line.split(" "):
+                        if word in wordDict:
+                                wordDict[word] += 1
+                        else:
+                                wordDict[word] = 1
 
-sorted_dict = sorted(wordDict.items(), key=lambda x:x[1], reverse=True)
+dict_items = wordDict.items()
+sorted_items = sorted(dict_items)
+sorted_dict = sorted(sorted_items, key=lambda x:x[1], reverse=True)
 
-print()
+newSort = {}
 for i in sorted_dict[0:5]:
-	print(i[0],':', i[1])
+        print(i[0], ":", i[1])
